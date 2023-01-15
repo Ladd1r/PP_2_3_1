192ff7d47b81b5d3e0bbf117d889d4b653b2f12e
Ladd1r <laddir42@gmail.com>
2023-01-15 18:19:39 +0700

    Made task

---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,42 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@Transactional
+public class UserDaoImpl implements UserDao{
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("from User", User.class).getResultList();
+    }
+
+    @Override
+    public void addUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public User getUser(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public void removeUser(int id) {
+        User user = entityManager.find(User.class, id);
+        entityManager.remove(user);
+    }
+}
---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,17 @@
+package web.service;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+
+    void addUser(User user);
+
+    User getUser(int id);
+
+    void updateUser(User user);
+
+    void removeUser(int id);
+}
